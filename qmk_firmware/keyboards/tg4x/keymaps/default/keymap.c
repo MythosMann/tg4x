fb18aabfb28e1b2df6400bd8dcce461dc0b340dd
Isaac Blackburn <isaac@tblackburn.me>
2019-01-23 14:42:56 -0500

    default keymap

--- qmk_firmware/keyboards/tg4x/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/tg4x/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2018 REPLACE_WITH_YOUR_NAME
+/* Copyright 2018 u/mythosmann
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#define _BL 0
+#define _FL 1
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
@@ -22,12 +24,29 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
-    KC_A,  KC_1,  KC_H, \
-      KC_TAB,  KC_SPC   \
-  ),
+/*
+,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
+|  Tab  |   q   |   w   |   e   |   r   |   t   |   y   |   u   |   i   |   o   |   p   |   '   | Bspc  |
+|-------'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-------|
+|  Caps   |   a   |   s   |   d   |   f   |   g   |   h   |   j   |   k   |   l   |   ;   |    Enter    |
+|---------'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----------|
+|  Shift     |   z   |   x   |   c   |   v   |   b   |   n   |   m   |   ,   |   .   |   /   |   Shift  |
+|-------,----'--,----'--,----'-------'-------'-------'-------'-------'--,----'--,----'--,----'--,-------|
+|  Ctl  |  Alt  |  Win  |                                               |  Fn   | Left  |  Up   | Right |
+`-------'-------'-------'-----------------------------------------------'-------'-------'-------'-------'
+*/
+  [_BL] = LAYOUT( /* Base */
+//,-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.-------.
+   KC_GESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  , KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,KC_RBRC, \
+//|-------'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-----'-,-------------|
+   KC_CAPS  , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,    KC_ENT   , \
+//|---------'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'--,----'-----,-'-------------|
+    KC_LSFT    , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_N  , KC_M  ,KC_COMM, KC_DOT,KC_SLSH, KC_RSFT  , \
+//|-------,----'--,----'--,----'-------'-------'-------'-------'-------'--,----'--,----'--,----'--,-------|
+   KC_LCTL,KC_LALT,KC_TAB,         KC_SPC                                ,MO(_FL),KC_LEFT, KC_UP ,KC_RGHT \
+//`-------'-------'-------'-----------------------------------------------'-------'-------'-------'-------'
+),
 };
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QMKBEST:
@@ -55,9 +74,5 @@ void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
 
 }
