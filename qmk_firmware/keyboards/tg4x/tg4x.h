4cb717feb2550643a8941cc12182b1cd621dce53
Isaac Blackburn <isaac@tblackburn.me>
2019-01-23 15:54:19 -0500

    fixed matrix naming

--- qmk_firmware/keyboards/tg4x/tg4x.h
+++ qmk_firmware/keyboards/tg4x/tg4x.h
@@ -30,13 +30,13 @@
       K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
        K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
         K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-      K30, K31, K32,                K33,         K34, K35, K36, K37, \
+      K30, K31, K32,                K33,         K34, K35, K36, K37 \
   ) \
   { \
-      { K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B,  K2C }, \
-      { K30,  K31,  K32,  K33,  K34,  K35,  K36,  K37,  K38,  K39,  K3A,  K3B,KC_NO, }, \
-      { K40,  K41,  K42,  K43,  K44,  K45,  K46,  K47,  K48,  K49,  K4A,  K4B,KC_NO, }, \
-      { K50,  K51,  K52,KC_NO,KC_NO,  K53,KC_NO,KC_NO,  K54,  K55,  K56,  K57,KC_NO, }, \
+      { K00,  K01,  K02,  K03,  K04,  K05,  K06,  K07,  K08,  K09,  K0A,  K0B,  K0C }, \
+      { K10,  K11,  K12,  K13,  K14,  K15,  K16,  K17,  K18,  K19,  K1A,  K1B,KC_NO }, \
+      { K20,  K21,  K22,  K23,  K24,  K25,  K26,  K27,  K28,  K29,  K2A,  K2B,KC_NO }, \
+      { K30,  K31,  K32,KC_NO,KC_NO,  K33,KC_NO,KC_NO,  K34,  K35,  K36,  K37,KC_NO }, \
   }
 
 #endif
